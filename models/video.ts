03e9b4c5d7d5f22d14fd648144c5255125d034e1
Harsh Mishra <mishraharsh.2803@gmail.com>
2025-10-19 16:52:31 +0530

    modelsdone

--- models/video.ts
+++ models/video.ts
@@ -1,4 +1,68 @@
 import mongoose from "mongoose";
 import { Schema, model, models } from "mongoose";
-import { use } from "react";
 
+export const VIDEO_DIMENSIONS = {
+  width: 1080,
+  heigth: 1920,
+} as const;
+
+export interface Ivideo {
+  _id?: mongoose.Types.ObjectId;
+  title: string;
+  discription: string;
+  videoUrl: string;
+  thumbnailUrl: string;
+  controls?: boolean;
+  transformations?: {
+    height: number;
+    width: number;
+    quality?: number;
+  };
+}
+
+const videoSchema = new Schema<Ivideo>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    discription: {
+      type: String,
+      required: true,
+    },
+    videoUrl: {
+      type: String,
+      required: true,
+    },
+    thumbnailUrl: {
+      type: String,
+      required: true,
+    },
+    controls: {
+      type: Boolean,
+      default: true,
+    },
+    transformations: {
+      height: {
+        type: Number,
+        required: false,
+        default: VIDEO_DIMENSIONS.heigth,
+      },
+      width: {
+        type: Number,
+        required: false,
+        default: VIDEO_DIMENSIONS.width,
+      },
+      quality: {
+        type: Number,
+        min: 1,
+        max: 100,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const video = models?.video || model<Ivideo>("video", videoSchema);
+export default video;
