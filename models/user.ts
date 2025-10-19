af46f90e2b9301e47865d2c38e5bb856518d9b89
Harsh Mishra <mishraharsh.2803@gmail.com>
2025-10-19 16:40:12 +0530

    usermodels

---
+++ models/user.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import { Schema, model, models } from "mongoose";
+import bcrypt from "bcryptjs";
+import { use } from "react";
+
+export interface Iuser {
+  email: string;
+  password: string;
+  _id?: mongoose.Types.ObjectId;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const userSchema = new Schema<Iuser>(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+const user = models?.user || model<Iuser>("user",userSchema);
+export default user ; 
---
+++ models/video.ts
@@ -0,0 +1,4 @@
+import mongoose from "mongoose";
+import { Schema, model, models } from "mongoose";
+import { use } from "react";
+
